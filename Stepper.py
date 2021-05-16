174bbf3f5866ed225bfc8c7445831ad24307026e
Dr-Weasel <83171418+Dr-Weasel@users.noreply.github.com>
2021-05-16 21:54:15 +0200

    Removed unnecessary functions, optimized execute_steps and execure_ramp

--- Stepper.py
+++ Stepper.py
@@ -28,8 +28,8 @@ class Stepper:
         
         self.freq_hi = steps_per_rev * rpm_hi / 60  # Hz
         self.freq_lo = steps_per_rev * rpm_lo / 60  # Hz
-        self.half_period_hi = int(5e5 / self.freq_hi) # half period time in us for freq_hi
-        self.half_period_lo = int(5e5 / self.freq_lo) # half period time in us for freq_lo
+        self.period_hi = int(1e6 / self.freq_hi) # half period time in us for freq_hi
+        self.period_lo = int(1e6 / self.freq_lo) # half period time in us for freq_lo
         self.steps_per_rev = steps_per_rev
         
 #         self.ramp_correction_factor_hi = 1.2 # for 1500 rpm with 800 steps per revolution
@@ -71,11 +71,11 @@ class Stepper:
         if steps != 0:            
             if steps_without_ramp > 0: # enough steps for higher speed and ramps
                 performed_steps_up = self.execute_ramp(self.ramp_up)
-                performed_steps_const = self.execute_steps(steps_without_ramp, self.half_period_hi)
+                performed_steps_const = self.execute_steps(steps_without_ramp, self.period_hi)
                 if self.ramp_dn:
                     performed_steps_dn = self.execute_ramp(self.ramp_dn)
             else: # not enough steps for higher speed and ramps
-                performed_steps_const = self.execute_steps(steps, self.half_period_lo)
+                performed_steps_const = self.execute_steps(steps, self.period_lo)
             number_of_performed_steps = (performed_steps_up + performed_steps_const + performed_steps_dn) * (1 if self.turn_right else -1)
         return number_of_performed_steps
     
@@ -85,27 +85,12 @@ class Stepper:
     def steps_to_revolutions(self, steps):
         return steps / self.steps_per_rev
     
-    def rel_angle(self, angle):
-        """Rotate stepper for given relative angle."""
-        steps = int(angle / 360 * self.steps_per_rev)
-        self.steps(steps)
-
-    def abs_angle(self, angle):
-        """Rotate stepper for given absolute angle since last power on."""
-        steps = int(angle / 360 * self.steps_per_rev)
-        steps -= self.current_position % self.steps_per_rev
-        self.steps(steps)
-
-    def revolution(self, rev_count):
-        """Perform given number of full revolutions."""
-        self.steps(rev_count * self.steps_per_rev)
-
     def calc_ramp(self, freq_start, freq_end, ramp_time):
         """
         Calculates a ramp from freq_start (in Hz) to freq_end (in Hz) within the ramp_time (in ms).
         
         Returns:
-        A list of integers of half period times (in us) from freq_start to freq_end
+        A list of integers of period times (in us) from freq_start to freq_end
         """
         
         t_ramp = ramp_time * 1000
@@ -113,41 +98,40 @@ class Stepper:
         b = freq_start
         x = 0
         correction = self.ramp_correction_factor
-        half_period_times = []
+        period_times = []
         
         while x < t_ramp:
             # calculating half period time in us
-            y = 5e5 / (m * x + b) * correction
-            half_period_times.append(int(y))
+            y = 1e6 / (m * x + b) * correction
+            period_times.append(int(y))
             x += 2 * y 
-        return half_period_times
+        return period_times
     
-    def execute_ramp(self, half_period_times):
+    def execute_ramp(self, period_times):
         """
-        Executes a ramp with a list of [x, y] with x from start of ramp_time to end of ramp_time and y from step_freq_start to step_freq_end
+        Executes a ramp with a list of period times from freq_start to freq_end
         """
-        
-        for index, half_period_time in enumerate(half_period_times):
-            if self.stop.value() == 0:
+        for index, period_time in enumerate(period_times):
+            before_if = ticks_us()
+            if self.stop.value() == 0 or self.stop.value() == 0 or self.stop.value() == 0:
                 return index # number of performed steps
+            after_if = ticks_us()
             self.stp.value(1)
-            sleep_us(half_period_time)
+            sleep_us(1)
             self.stp.value(0)
-            sleep_us(half_period_time)
-            #if self.turn_right:
-            #    self.current_steps +=1
-            #else:
-            #    self.current_steps -=1
-        return len(half_period_times)
+            sleep_us(period_time + before_if - after_if - 1)
+        return len(period_times)
     
-    def execute_steps(self, steps, half_period_time):
+    def execute_steps(self, steps, period_time):
         for i in range(steps):
-            if self.stop.value() == 0:
-                return i            
+            before_if = ticks_us()
+            if self.stop.value() == 0 or self.stop.value() == 0 or self.stop.value() == 0:
+                return i
+            after_if = ticks_us()
             self.stp.value(1)
-            sleep_us(half_period_time)
+            sleep_us(1)
             self.stp.value(0)
-            sleep_us(half_period_time)
+            sleep_us(period_time + before_if - after_if - 1)
         return steps
         
         
@@ -156,25 +140,10 @@ if __name__ == "__main__":
     stepper_dir = Pin(3)
     stepper_en = Pin(4)
     # def __init__(self, step_pin, dir_pin, sleep_pin, rpm_hi, rpm_lo, ramp_up_time, ramp_dn_time, steps_per_rev)
-    m1 = Stepper(stepper_pul, stepper_dir, stepper_en, 600, 50, 500, 400, 800)
-    print(m1.do_revolutions(-100))
+    m1 = Stepper(stepper_pul, stepper_dir, stepper_en, 600, 50, 1200, 400, 800)
+    for i in range(10):
+        m1.do_revolutions(4)
+        sleep_ms(50)
+        m1.do_revolutions(-4)
+        sleep_ms(50)
     
-    
-    #m1.steps(9000)
-    #m1.ramp(800, m1.step_freq, 3000)
-    #m1.steps(10000)
-    #steps_calc = m1.ramp_step_forecast(500, m1.step_time, 1000)
-    #results_from_ramp = m1.ramp(400, m1.step_freq, 500)
-    #steps_actual = results_from_ramp[0]
-    #ramp_x_y = results_from_ramp[1]
-    #print("calculated steps: ", steps_calc)
-    #print("actual steps: ", steps_actual)
-    #m1.steps(steps - steps_ramp)
-    
-    #m1.steps(-40000)
-#     data = ""
-#     for i in ramp_x_y:
-#         data += str(i[0]) + "," + str(i[1]) + "\r\n"
-#     f = open('data.csv', 'w')
-#     f.write(data)
-#     f.close()
